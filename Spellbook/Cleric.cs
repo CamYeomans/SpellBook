8be6dbe5099cf9e029ebea966ee91ba322a70712
camyeomans <cameron.yeomans2016@gmail.com>
2019-08-13 19:28:57 -0600

    added cleric class

---
+++ Spellbook/Cleric.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spellbook
+{
+    class Cleric : CharacterClass
+    {
+
+        public Cleric(int playerLvl)
+        {
+            
+        }
+
+        public override int getTotalSpellsKnown(int classLevel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
